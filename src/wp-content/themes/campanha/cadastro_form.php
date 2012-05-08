2e2980d01ef0b8770bc57162145d274e2c8eab19
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2012-05-08 11:28:31 -0300

    mantem o valor dos campos do cadastro quando ocorre um erro de validacao

--- src/wp-content/themes/campanha/cadastro_form.php
+++ src/wp-content/themes/campanha/cadastro_form.php
@@ -2,15 +2,15 @@
     <input type="hidden" name="action" value="register" />
     <div class="fields">
         <label for="userinput">usuário</label>
-        <input id="userinput" class="alignleft" type="text" name="user_login" value="" />
+        <input id="userinput" class="alignleft" type="text" name="user_login" value="<?php if (isset($_POST['user_login'])) echo $_POST['user_login']; ?>" />
     </div>
     <div class="fields">
         <label for="emailinput">email</label>
-        <input id="emailinput" class="alignleft" type="email" name="user_email" value="" />
+        <input id="emailinput" class="alignleft" type="email" name="user_email" value="<?php if (isset($_POST['user_email'])) echo $_POST['user_email']; ?>" />
     </div>
     <div class="fields">
         <label for="password">senha</label>
-        <input id="password" class="alignleft" type="password" name="user_pass" />
+        <input id="password" class="alignleft" type="password" name="user_pass" value="<?php if (isset($_POST['user_pass'])) echo $_POST['user_pass']; ?>"/>
     </div>
     <input type="submit" value="Cadastrar" class="green" />
 </form>
