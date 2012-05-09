b6e549df459c947ff0125c71dd842382ba094a51
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2012-05-09 17:35:46 -0300

    limpa interface do admin apenas para usuarios do grupo subscriber

--- src/wp-content/themes/campanha/custom_admin.php
+++ src/wp-content/themes/campanha/custom_admin.php
@@ -1,6 +1,8 @@
 <?php
 
-if (current_user_can('subscriber')) {
+$user = wp_get_current_user();
+
+if (!empty($user->roles) && is_array($user->roles) && in_array('subscriber', $user->roles)) {
     add_action('wp_dashboard_setup', 'campanha_remove_dashboard_widgets');
     
     add_action('admin_init', function() {
@@ -20,18 +22,15 @@ if (current_user_can('subscriber')) {
         add_filter('contextual_help_list', function() {
             get_current_screen()->remove_help_tabs();
         });
-    });
-    
-    add_action('wp_dashboard_setup', function() {
-        // add widget to list user's campaigns to the dashboard 
-        wp_add_dashboard_widget('user_campaigns', 'Suas campanhas', 'campanha_user_campaigns_widget');
+
+        // remove default menu options        
+        remove_menu_page('index.php');
+        remove_menu_page('profile.php');
         
-        // add widget to start the creation of a new campaign to the dashboard 
-        wp_add_dashboard_widget('create_campaign', 'Criar nova campanha', 'campanha_create_new_campaign_widget');
+        add_menu_page('Administrar campanhas', 'Administrar campanhas', 'read', 'campaigns.php', function() {
+            require(TEMPLATEPATH . '/campaigns.php');
+        });
     });
-    
-    // remove default admin menu options
-    add_action('admin_head', 'remove_menus');
 }
 
 /**
@@ -47,32 +46,4 @@ function campanha_remove_dashboard_widgets() {
     unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
     unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
     unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
-}
-
-/**
- * Print user's campaigns admin widget
- */
-function campanha_user_campaigns_widget() {
-    echo 'AHA!';
-}
-
-/**
- * Widget to start the creation of a new campaign
- */
-function campanha_create_new_campaign_widget() {
-    echo 'BUH!';
-}
-
-/**
- * Remove default menu options
- */
-function remove_menus() {
-    global $menu;
-    $restricted = array(__('Dashboard'));
-    foreach ($menu as $key => $value) {
-        $value = explode(' ', $menu[$key][0]);
-        if (in_array($value[0] != NULL ? $value[0] : "" , $restricted)){
-            unset($menu[$key]);
-        }
-    }
 }
