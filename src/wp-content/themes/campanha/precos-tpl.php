9d276b87fd7fc155878f30fd715e07f40336d27d
Catia Kitahara <catia@hacklab.com.br>
2012-05-11 17:11:32 -0300

    tabela responsiva

--- src/wp-content/themes/campanha/precos-tpl.php
+++ src/wp-content/themes/campanha/precos-tpl.php
@@ -13,77 +13,79 @@ Template Name: Preços
 				</header>
 				<div class="post-content clearfix">
 					<?php the_content(); ?>
-					<table>
-						<thead>
+					<table class="clearfix">
+						<thead class="clearfix">
 							<th class="cel-4"></th>
 							<th class="cel-2">1</th>
 							<th class="cel-2">2</th>
 							<th class="cel-2">3</th>
 							<th class="cel-2">4</th>
 						</thead>
-						<tr>
-							<th class="feature">Site ou Blog</th>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr>
-							<th class="feature">Mobilização nas redes sociais</th>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr>
-							<th class="feature">Envio de email e SMS</th>
-							<td>5<span> mil envios</span></td>
-							<td>10<span> mil envios</span></td>
-							<td>20<span> mil envios</span></td>
-							<td>50<span> mil envios</span></td>
-						</tr>
-						<tr>
-							<th class="feature">Upload de arquivos</th>
-							<td>1G</td>
-							<td>2G</td>
-							<td>3G</td>
-							<td>ilimitado</td>
-						</tr>
-						<tr>
-							<th class="feature">Geração de material gráfico</th>
-							<td class="nao">não</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr>
-							<th class="feature">Gerenciamento de contatos</th>
-							<td class="nao">não</td>
-							<td class="nao">não</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr>
-							<th class="feature">Suporte via fórum</th>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr>
-							<th class="feature">Suporte por e-mail</th>
-							<td class="nao">não</td>
-							<td class="nao">não</td>
-							<td class="sim">sim</td>
-							<td class="sim">sim</td>
-						</tr>
-						<tr class="last">
-							<th class="feature">Valor anual</th>
-							<td class="valor">R$1.300,00</td>
-							<td class="valor">R$1.800,00</td>
-							<td class="valor">R$2.500,00</td>
-							<td class="valor">R$3.500,00</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th class="feature">Site ou Blog</th>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr>
+								<th class="feature">Mobilização nas redes sociais</th>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr>
+								<th class="feature">Envio de email e SMS</th>
+								<td>5<span> mil envios</span></td>
+								<td>10<span> mil envios</span></td>
+								<td>20<span> mil envios</span></td>
+								<td>50<span> mil envios</span></td>
+							</tr>
+							<tr>
+								<th class="feature">Upload de arquivos</th>
+								<td>1G</td>
+								<td>2G</td>
+								<td>3G</td>
+								<td>ilimitado</td>
+							</tr>
+							<tr>
+								<th class="feature">Geração de material gráfico</th>
+								<td class="nao">não</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr>
+								<th class="feature">Gerenciamento de contatos</th>
+								<td class="nao">não</td>
+								<td class="nao">não</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr>
+								<th class="feature">Suporte via fórum</th>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr>
+								<th class="feature">Suporte por e-mail</th>
+								<td class="nao">não</td>
+								<td class="nao">não</td>
+								<td class="sim">sim</td>
+								<td class="sim">sim</td>
+							</tr>
+							<tr class="last">
+								<th class="feature">Valor anual</th>
+								<td class="valor">R$1.300,00</td>
+								<td class="valor">R$1.800,00</td>
+								<td class="valor">R$2.500,00</td>
+								<td class="valor">R$3.500,00</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 				<!-- .post-content -->
