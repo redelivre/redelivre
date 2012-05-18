eddf4cfe763e7c6fbcd0656ec4f598221840d857
Leo Germani <leogermani@hacklab.com.br>
2012-05-18 17:30:21 -0300

    metodos novos no Plan

--- src/wp-content/mu-plugins/campaign_base/Plan.php
+++ src/wp-content/mu-plugins/campaign_base/Plan.php
@@ -1,6 +1,32 @@
 <?php
 
 class Plan {
+    
+    
+    /**
+     * Plan id
+     * @var int
+     */
+    public $id;
+    
+    /**
+     * Election id
+     * @var int
+     */
+    public $election_id;
+    
+    /**
+     * Plan name
+     * @var string
+     */
+    public $state;
+    
+    /**
+     * Plan price
+     * @var int
+     */
+    public $price;
+    
     /**
      * Return all available plans
      * 
@@ -33,4 +59,40 @@ class Plan {
             
         return $name;
     }
+    
+    /**
+     * Get a plan by id
+     * 
+     * @param int $id
+     * @return Plan
+     */
+    public static function getById($id) {
+        global $wpdb;
+        
+        $result = $wpdb->get_row($wpdb->prepare("SELECT * FROM `plans` WHERE id = %d", $id), ARRAY_A);
+        
+        return new Plan($result);
+    }
+    
+    public function __construct(array $data) {
+        
+
+        if (isset($data['election_id'])) {
+            $this->election_id = $data['election_id'];
+        }
+        
+        if (isset($data['name'])) {
+            $this->name = $data['name'];
+        }
+  
+        if (isset($data['id'])) {
+            $this->id = $data['id'];
+        }
+        
+        if (isset($data['price'])) {
+            $this->price = $data['price'];
+        }
+        
+    }
+    
 }
