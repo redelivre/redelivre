bf195c12dfd937f213175a68e028b9fe26653920
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-05-21 15:17:20 -0300

    usa hook correto para adicionar itens ao menu do admin

--- src/wp-content/themes/campanha/custom_admin.php
+++ src/wp-content/themes/campanha/custom_admin.php
@@ -3,9 +3,7 @@
 $user = wp_get_current_user();
 
 if (!empty($user->roles) && is_array($user->roles) && in_array('subscriber', $user->roles)) {
-    add_action('init', function() {
-        // couldn't make the call to add_menu_page() work without first including the file below
-        require(ABSPATH . '/wp-admin/includes/plugin.php');
+    add_action('admin_menu', function() {
         add_menu_page('Administrar campanhas', 'Administrar campanhas', 'read', 'campaigns', function() {
             require(TEMPLATEPATH . '/campaigns.php');
         });
--- src/wp-content/themes/campanha_padrao/includes/load_menu_options.php
+++ src/wp-content/themes/campanha_padrao/includes/load_menu_options.php
@@ -3,24 +3,25 @@
 // add menu options according to plan capabilities
 $capabilities = Capability::getByPlanId($campaign->plan_id);
 
-// couldn't make the call to add_menu_page() work without first including the file below
-require(ABSPATH . '/wp-admin/includes/plugin.php');
-
-if ($capabilities->graphic_material->access) {
-    add_menu_page('Material gráfico', 'Material gráfico', 'read', 'graphic_material', function() {
-        require(TEMPLATEPATH . '/includes/graphic_material.php');
-    });
-}
-
-if ($capabilities->contact_manager->access) {
-    add_menu_page('Gerenciador de contatos', 'Gerenciador de contatos', 'read', 'contact_manager', function() {
-        require(TEMPLATEPATH . '/includes/contact_manager.php');
-    });
-}
-
-//TODO: check why the last menu entry is not being displayed
-if ($capabilities->forum_support->access || $capabilities->email_support->access) {
-    add_menu_page('Suporte', 'Suporte', 'read', 'campaign_support', function() {
-        require(TEMPLATEPATH . '/includes/campaign_support.php');
-    });
-}
+add_action('admin_menu', function() {
+    global $capabilities;
+    
+    if ($capabilities->graphic_material->access) {
+        add_menu_page('Material gráfico', 'Material gráfico', 'read', 'graphic_material', function() {
+            require(TEMPLATEPATH . '/includes/graphic_material.php');
+        });
+    }
+    
+    if ($capabilities->contact_manager->access) {
+        add_menu_page('Gerenciador de contatos', 'Gerenciador de contatos', 'read', 'contact_manager', function() {
+            require(TEMPLATEPATH . '/includes/contact_manager.php');
+        });
+    }
+    
+    //TODO: check why the last menu entry is not being displayed
+    if ($capabilities->forum_support->access || $capabilities->email_support->access) {
+        add_menu_page('Suporte', 'Suporte', 'read', 'campaign_support', function() {
+            require(TEMPLATEPATH . '/includes/campaign_support.php');
+        });
+    }
+});
