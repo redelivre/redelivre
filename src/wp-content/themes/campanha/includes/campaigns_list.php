f377b1c7c4e262a8bef6f9f7de1c4a031d148d99
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-06-15 10:17:15 -0300

    mensagem se o usuario nao tiver nenhuma campanha

--- src/wp-content/themes/campanha/includes/campaigns_list.php
+++ src/wp-content/themes/campanha/includes/campaigns_list.php
@@ -16,30 +16,34 @@ if (is_super_admin()) {
         <div class="updated"><p>Campanha criada com sucesso.</p></div>
     <?php endif; ?>        
     
-    <table class="widefat fixed">
-        <thead>
-            <tr class="thead">
-                <th>Sub-domínio</th>
-                <th>Domínio próprio</th>
-                <?php if (is_super_admin()) echo '<th>Usuário</th>'; ?>
-                <th>Número do candidato</th>
-                <th>Plano</th>
-                <th>Status</th>
-                <th>Data de criação</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($campaigns as $campaign): ?>
-                <tr>
-                    <td><a href="<?php echo $campaign->domain; ?>" target="_blank"><?php echo $campaign->domain ?></a> (<a href="<?php echo $campaign->domain; ?>/wp-admin" target="_blank">admin</a>)</td>
-                    <td><a href="<?php echo $campaign->own_domain; ?>" target="_blank"><?php echo $campaign->own_domain ?></a></td>
-                    <?php if (is_super_admin()) echo "<td>{$campaign->campaignOwner->data->user_login}</td>"; ?>
-                    <td><?php echo $campaign->candidate_number; ?></td>
-                    <td><?php echo Plan::getName($campaign->plan_id); ?></td>
-                    <td><?php echo $campaign->getStatus(); ?></td>
-                    <td><?php echo date('d/m/Y', strtotime($campaign->creation_date)); ?></td>
+    <?php if ($campaigns) : ?>
+        <table class="widefat fixed">
+            <thead>
+                <tr class="thead">
+                    <th>Sub-domínio</th>
+                    <th>Domínio próprio</th>
+                    <?php if (is_super_admin()) echo '<th>Usuário</th>'; ?>
+                    <th>Número do candidato</th>
+                    <th>Plano</th>
+                    <th>Status</th>
+                    <th>Data de criação</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($campaigns as $campaign): ?>
+                    <tr>
+                        <td><a href="<?php echo $campaign->domain; ?>" target="_blank"><?php echo $campaign->domain ?></a> (<a href="<?php echo $campaign->domain; ?>/wp-admin" target="_blank">admin</a>)</td>
+                        <td><a href="<?php echo $campaign->own_domain; ?>" target="_blank"><?php echo $campaign->own_domain ?></a></td>
+                        <?php if (is_super_admin()) echo "<td>{$campaign->campaignOwner->data->user_login}</td>"; ?>
+                        <td><?php echo $campaign->candidate_number; ?></td>
+                        <td><?php echo Plan::getName($campaign->plan_id); ?></td>
+                        <td><?php echo $campaign->getStatus(); ?></td>
+                        <td><?php echo date('d/m/Y', strtotime($campaign->creation_date)); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p>Você ainda não criou nenhuma campanha. Para isso vá para a <a href="<?php echo admin_url(CAMPAIGN_NEW_URL); ?>">página de criação de campanha</a>.</p>
+    <?php endif; ?>
 </div>
