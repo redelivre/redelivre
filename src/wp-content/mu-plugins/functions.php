6f57229f882bb04655033833812c5c38012e31dc
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-06-15 11:49:56 -0300

    muda as strings do menu de admin "meus sites"

--- src/wp-content/mu-plugins/functions.php
+++ src/wp-content/mu-plugins/functions.php
@@ -179,4 +179,8 @@ function campanha_add_common_js() {
     if (is_user_logged_in()) {
         wp_enqueue_script('uservoice', site_url() . '/wp-content/mu-plugins/js/uservoice.js', 'jquery');
     }
+    
+    if (is_admin()) {
+        wp_enqueue_script('campaign_common', site_url() . '/wp-content/mu-plugins/js/campaign_common.js', 'jquery');
+    }
 }
