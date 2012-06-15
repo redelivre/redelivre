0c8ebac1e351d054c9203f08ea4d7ebcbb859537
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-06-15 16:52:22 -0300

    remove temporariamente o link para pagamento

--- src/wp-content/mu-plugins/functions.php
+++ src/wp-content/mu-plugins/functions.php
@@ -124,7 +124,9 @@ function campanha_admin_payment_message() {
     
     if (!$campaign->isPaid()) {
         $link = admin_url('admin.php?page=payments');
-        echo "<div class='error'><p>Está campanha está visível somente para o criador pois o pagamento está pendente. <a href='$link'>Pague agora!</a></p></div>";
+        //echo "<div class='error'><p>Está campanha está visível somente para o criador pois o pagamento está pendente. <a href='$link'>Pague agora!</a></p></div>";
+        // temporarily remove link to payment page while it is not finished
+        echo "<div class='error'><p>Está campanha está visível somente para o criador pois o pagamento está pendente.</p></div>";
     }
 }
 
