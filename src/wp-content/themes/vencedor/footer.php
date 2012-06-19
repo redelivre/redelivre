8408d8cc1c345ce8581e08ccc18fc3a0d4a356ff
Catia Kitahara <catia@hacklab.com.br>
2012-06-19 15:28:47 -0300

    licença e estilos do editor

--- src/wp-content/themes/vencedor/footer.php
+++ src/wp-content/themes/vencedor/footer.php
@@ -1,6 +1,6 @@
 	<footer id="main-footer" class="wrap clearfix">					
 		<?php wp_nav_menu( array( 'theme_location' => 'rodape', 'container' => false, 'menu_id' => 'footer-nav', 'menu_class' =>'col-8 clearfix', 'fallback_cb' => '', 'depth' =>'1' ) ); ?>
-		<p class="creditos textright col-4">Tema Vencedor &bull; <a href="http://campanhacompleta.com.br" title="Campanha Completa"><img src="<?php bloginfo( 'template_url' ); ?>/img/campanha-completa.png" alt="" /></a> &bull; <a href="http://wordpress.org"><img src="<?php bloginfo( 'template_url' ); ?>/img/wp.png" alt="" /></a></p>
+		<p class="creditos textright col-4">Tema Vencedor &bull; <a href="http://campanhacompleta.com.br" title="Campanha Completa"><img src="<?php echo get_template_directory_uri(); ?>/img/campanha-completa.png" alt="" /></a> &bull; <a href="http://wordpress.org"><img src="<?php echo get_template_directory_uri(); ?>/img/wp.png" alt="" /></a></p>
 	</footer>
 	<!-- #main-footer -->
 <?php wp_footer(); ?>
--- src/wp-content/themes/vencedor/header.php
+++ src/wp-content/themes/vencedor/header.php
@@ -23,16 +23,16 @@ $sidebar = get_option('vencedor_theme_options');
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+		echo ' | Página' . max( $paged, $page );
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <!--[if lte IE 7]>
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>/ie-hacks.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri() ?>/ie-hacks.css" />
 <![endif]-->
 <!--[if lt IE 9]>
-	<script src="<?php bloginfo('template_url'); ?>/js/html5.js" type="text/javascript"></script>
+	<script src="<?php get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php if ($sidebar): ?>
@@ -50,14 +50,14 @@ $sidebar = get_option('vencedor_theme_options');
 <body <?php body_class(); ?>>
 <header id="main-header">
 	<div id="branding" class="wrap clearfix">
-		<h1 class="col-12"><span><a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a></span></h1>
+		<h1 class="col-12"><span><a href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a></span></h1>
 		<p id="description" class="col-12"><?php bloginfo( 'description' ); ?></p>			
 	</div>
 	<!-- .wrap -->
 	<div id="menubar">
 		<div class="wrap clearfix">
 			<?php wp_nav_menu( array( 'menu' => 'main', 'theme_location' => 'main', 'container' => '', 'menu_id' => 'main-nav', 'menu_class' => 'clearfix', 'fallback_cb' => '', 'depth' => '3',) ); ?>
-			<div id="feed-link"><a href="<?php bloginfo('rss_url'); ?>" title="RSS Feed"><img src="<?php bloginfo('template_url'); ?>/img/feed-icon-24x24.png" /></a></div>
+			<div id="feed-link"><a href="<?php bloginfo('rss_url'); ?>" title="RSS Feed"><img src="<?php echo get_template_directory_uri(); ?>/img/feed-icon-24x24.png" /></a></div>
 		</div>
 		<!-- .wrap -->
 	</div>
