cb90c00b177eb7100aad5f3a155dd432521d2d9b
Rafael Freitas <rafael.chaves.freitas@gmail.com>
2012-06-25 19:55:21 -0300

    Facebook like box

--- src/wp-content/mu-plugins/includes/admin-social-networks.php
+++ src/wp-content/mu-plugins/includes/admin-social-networks.php
@@ -8,7 +8,7 @@ if (isset($_POST['admin_social_networks']) && wp_verify_nonce($_POST['admin_soci
 $option = get_option('campanha_social_networks');
 ?>
 <style>
-    label { display:inline-block; width:100px; }
+    label { display:inline-block; width:150px; }
     input[type="text"] { width: 350px; }
     
 </style>
@@ -19,7 +19,9 @@ $option = get_option('campanha_social_networks');
         <?php echo $msg; ?>
         <?php wp_nonce_field('admin_social_networks', 'admin_social_networks'); ?>
         <p>
-            <label for="rede-1">Facebook:</label> <input id="rede-1" type="text" name="social_networks[facebook]" value="<?php echo @htmlentities($option['facebook']) ?>"/><br/>
+            <label for="rede-0">Página do Facebook:</label> <input id="rede-0" type="text" name="social_networks[facebook-page]" value="<?php echo @htmlentities($option['facebook-page']) ?>"/><br/>
+
+            <label for="rede-1">Parfil do Facebook:</label> <input id="rede-1" type="text" name="social_networks[facebook]" value="<?php echo @htmlentities($option['facebook']) ?>"/><br/>
 
             <label for="rede-2">Twitter:</label> <input id="rede-2" type="text" name="social_networks[twitter]" value="<?php echo @htmlentities($option['twitter']) ?>"/><br/>
 
