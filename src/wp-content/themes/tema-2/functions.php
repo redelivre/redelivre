c932738573b1ad43c01e0d7e7cb6a92ac5a23062
Catia Kitahara <catia@hacklab.com.br>
2012-06-27 09:56:13 -0300

    removendo funcoes para esconder barra de admin

--- src/wp-content/themes/tema-2/functions.php
+++ src/wp-content/themes/tema-2/functions.php
@@ -56,11 +56,11 @@ function tema2_setup() {
 // admin_bar removal
 //wp_deregister_script('admin-bar');
 //wp_deregister_style('admin-bar');
-remove_action('wp_footer','wp_admin_bar_render',1000);
-function remove_admin_bar(){
-   return false;
-}
-add_filter( 'show_admin_bar' , 'remove_admin_bar');
+//remove_action('wp_footer','wp_admin_bar_render',1000);
+//function remove_admin_bar(){
+//   return false;
+//}
+//add_filter( 'show_admin_bar' , 'remove_admin_bar');
 
 // JS
 add_action('wp_print_scripts', 'tema2_addJS');
