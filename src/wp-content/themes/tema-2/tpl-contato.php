289f0d430097b334b3c1f86edb40815ea347d35e
Catia Kitahara <catia@hacklab.com.br>
2012-06-27 11:27:51 -0300

    template de contato

---
+++ src/wp-content/themes/tema-2/tpl-contato.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+		<?php get_sidebar(); ?>
+		<section id="main-section" class="col-8">			
+			<?php if (get_option('campanha_contact_enabled')) : ?>
+				<article id="post-<?php the_ID(); ?>" class="clearfix"> 	  
+					<header>                       
+						<h1>Entre em contato</h1>				
+					</header>
+					<div class="post-content clearfix">
+						<?php if (function_exists('campanha_the_contact_form')) campanha_the_contact_form(); ?>	
+					</div>
+					<!-- .post-content -->
+				</article>
+				<!-- .page -->			
+			<?php else : ?>
+			   <p><p>Essa página está desabilitada</p></p>              
+			<?php endif; ?>
+		</section>
+		<!-- #main-section -->   
+<?php get_footer(); ?>
