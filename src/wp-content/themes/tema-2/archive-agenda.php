31e27e9bc3a5baee7f997b702c7d4b0dfbc2d8d3
Catia Kitahara <catia@hacklab.com.br>
2012-06-28 15:28:23 -0300

    agenda no tema 2

---
+++ src/wp-content/themes/tema-2/archive-agenda.php
@@ -0,0 +1,37 @@
+<?php
+
+global $paged;
+$showingPast = ($paged > 0 || $_GET['eventos'] == 'passados');
+?>
+<?php get_header(); ?>
+    <?php get_sidebar(); ?>
+    <section id="main-section" class="col-8">
+		<?php if ($showingPast): ?>
+			<h2 class="clearfix">
+				Eventos Passados
+				<a class="view-events" href="<?php echo add_query_arg('eventos', ''); ?>">Ver próximos eventos &raquo;</a>
+			</h2>
+		<?php else: ?>
+			<h2 class="clearfix">
+				Próximos eventos
+				<a class="view-events" href="<?php echo add_query_arg('eventos', 'passados'); ?>">Ver eventos passados &raquo;</a>
+			</h2>
+		<?php endif; ?>
+        <?php if (have_posts()) : ?>
+			
+        <?php while (have_posts()) : the_post(); ?>
+            <?php html::part('loop','agenda'); ?>
+        <?php endwhile; ?>
+            <?php if ($wp_query->max_num_pages > 1) : ?>
+                <nav id="posts-nav" class="clearfix">
+                    <div class="alignleft"><?php next_posts_link(__('&laquo; Previous posts', 'tema2')); ?></div>
+                    <div class="alignright"><?php previous_posts_link(__('Next posts &raquo;', 'tema2')); ?></div>
+                </nav>
+                <!-- #posts-nav -->
+            <?php endif; ?>					
+        <?php else : ?>
+            <p><?php _e('No results found.', 'tema2'); ?></p>              
+        <?php endif; ?>
+    </section>
+    <!-- #main-section -->
+<?php get_footer(); ?>
---
+++ src/wp-content/themes/tema-2/single-agenda.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+		<?php get_sidebar(); ?>
+		<section id="main-section" class="col-8">			
+			<?php if ( have_posts()) : the_post(); ?>
+				<?php $meta = get_metadata('post', $post->ID); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix');?>>	  
+					<header>                       
+						<h1><?php the_title();?></h1>					
+					</header>
+					<div class="post-content clearfix">
+						<?php if (has_post_thumbnail()) : ?> 
+							<?php the_post_thumbnail('medium'); ?>				 
+						<?php endif; ?>
+						<?php the_content(); ?>
+						<div class="event-info clear">
+							<h3>Informações do Evento</h3>
+							<?php
+							echo '<p class="bottom"><span class="label">Data Inicial:</span> ', date('d/m/Y', strtotime($meta['_data_inicial'][0])), '</p>';
+							echo '<p class="bottom"><span class="label">Data Final:</span> ', date('d/m/Y', strtotime($meta['_data_final'][0])), '</p>';
+							echo '<p class="bottom"><span class="label">Local:</span> ', $meta['_onde'][0], '</p>';
+							echo '<p class="bottom"><span class="label">Site:</span> ', $meta['_link'][0], '</p>';
+							?>
+						</div>		
+					</div>
+					<!-- .post-content -->
+					<footer class="clearfix">
+						<?php html::part("interaction");?>							
+					</footer>
+				</article>
+				<!-- .post -->
+				<nav id="posts-nav" class="clearfix">
+					<a href="<?php bloginfo('url'); ?>/agenda?eventos">Ver próximos eventos</a> | <a href="<?php bloginfo('url'); ?>/agenda?eventos=passados">Ver eventos passados</a>
+				</nav>		
+			<?php else : ?>
+			   <p><?php _e('No results found.', 'tema2'); ?></p>              
+			<?php endif; ?>
+		</section>
+		<!-- #main-section -->
+<?php get_footer(); ?>
