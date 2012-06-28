9654ad83b8ca2531805268fcf461076f8dc18a36
Catia Kitahara <catia@hacklab.com.br>
2012-06-28 16:18:48 -0300

    ajustei a tabela de preços do tema campanha

--- src/wp-content/themes/campanha/includes/campaigns_prices.php
+++ src/wp-content/themes/campanha/includes/campaigns_prices.php
@@ -17,12 +17,12 @@
         <td class="sim textcenter"><?php html::image("sim.png","Sim"); ?></td>
     </tr>
     <tr>
-        <th class="feature textleft">Envio de email e SMS</th>
+        <th class="feature textleft">Envios de email e SMS</th>
         <td class="nao textcenter"><?php html::image("nao.png","Não"); ?></td>
-        <td class="textcenter">5<span> mil envios</span></td>
-        <td class="textcenter">10<span> mil envios</span></td>
-        <td class="textcenter">20<span> mil envios</span></td>
-        <td class="textcenter">50<span> mil envios</span></td>
+        <td class="textcenter">5<span> mil</span></td>
+        <td class="textcenter">10<span> mil</span></td>
+        <td class="textcenter">20<span> mil</span></td>
+        <td class="textcenter">50<span> mil</span></td>
     </tr>
     <tr>
         <th class="feature textleft">Upload de arquivos</th>
