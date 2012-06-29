e1f1fc1a40c67a721baf1b4f7a5d024c77693184
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-06-29 14:56:18 -0300

    indentacao

--- src/wp-content/mu-plugins/functions.php
+++ src/wp-content/mu-plugins/functions.php
@@ -108,17 +108,17 @@ function campaign_base_template_redirect_intercept() {
             die;
         case 'mobilizacao':
             add_action('wp_print_scripts', function() {
-                        wp_enqueue_script('contato', WPMU_PLUGIN_URL . '/js/mobilize.js');
-                    });
+                wp_enqueue_script('contato', WPMU_PLUGIN_URL . '/js/mobilize.js');
+            });
 
             require(WPMU_PLUGIN_DIR . '/includes/tpl-mobilize.php');
             die;
         case 'contato':
             add_action('wp_print_scripts', function() {
-                        wp_enqueue_script('jquery_validate', WPMU_PLUGIN_URL . '/js/jquery.validate.min.js', array('jquery'));
-                        wp_enqueue_script('contato', WPMU_PLUGIN_URL . '/js/contato.js', array('jquery_validate'));
-                        wp_localize_script('contato', 'vars', array('ajaxurl' => admin_url('admin-ajax.php')));
-                    });
+                wp_enqueue_script('jquery_validate', WPMU_PLUGIN_URL . '/js/jquery.validate.min.js', array('jquery'));
+                wp_enqueue_script('contato', WPMU_PLUGIN_URL . '/js/contato.js', array('jquery_validate'));
+                wp_localize_script('contato', 'vars', array('ajaxurl' => admin_url('admin-ajax.php')));
+            });
 
             // template específico dentro do tema
             if (file_exists(STYLESHEETPATH . '/tpl-contato.php')) { // tema filho
