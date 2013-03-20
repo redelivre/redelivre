9d35f86434735908a94971de429192413a9a9af5
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-03-20 14:32:52 -0300

    adiciona o site na mensagem enviada no formulario de contato (fixes #1220)

--- src/wp-content/mu-plugins/includes/admin-contact.php
+++ src/wp-content/mu-plugins/includes/admin-contact.php
@@ -121,8 +121,9 @@ add_action('wp_ajax_form-contato', 'campanha_handle_form_contato');
 add_action('wp_ajax_nopriv_form-contato', 'campanha_handle_form_contato');
 
 function campanha_handle_form_contato() {
-
-    $msg = '';
+    global $current_blog;    
+    
+    $msg = "site: " . get_site_url($current_blog->blog_id) . "\n";
     
     foreach($_POST as $campo => $valor) {
     
