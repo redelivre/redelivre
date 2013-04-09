ed8126e53a0ccce64a07d01e84dc7c0ed4a542b6
Jacson Passold <jacson@ethymos.com.br>
2013-04-09 00:46:14 -0300

    Ultima alterações do consulta

--- src/wp-content/themes/consultas-maracana/comments-objects.php
+++ src/wp-content/themes/consultas-maracana/comments-objects.php
@@ -42,25 +42,36 @@ add_filter('comment_class','comment_add_microid');
    
     
     <!--show the form-->
+    
+    
+   
     <?php if('open' == $post->comment_status && !is_consulta_encerrada()) : ?>
     <div id="respond" class="clearfix">
         <h5><?php _oi('Deixe seu comentário', 'Comentários: título do formulário'); ?></h5>
+        <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="form-comentario" class="clearfix">
         <?php if( !is_user_logged_in()) : ?>
-            
-            <p>
-            
-            <?php printf( __( 'Você precisa fazer <a href="%s">login</a> para publicar um comentário.', 'consulta' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( get_the_ID() ) ) ) ); ?> 
-            
-            </p>
+        <p>
+        <label for="author">Nome:</label>
+        <input id="author" type="text" aria-required="true" tabindex="1" size="22" value="" name="author">
+        </p>
+        <p>
+        <label for="email">Email:</label>
+        <input id="email" type="text" aria-required="true" tabindex="2" size="22" value="" name="email">
+        </p>
+        <p>
+        <label for="url">Site:</label>
+        <input id="url" type="text" tabindex="3" size="22" value="" name="url">
+        </p>
         
         <?php else : ?>
-        <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="form-comentario" class="clearfix">
+             <?php if($user_ID) : ?>
+                <p>Conectado como <a href="<?php print get_option('siteurl'); ?>/wp-admin/profile.php"><?php print $user_identity; ?></a>. <a href="<?php print get_option('siteurl'); ?>/wp-login.php?action=logout" title="Logout">Logout &raquo;</a></p>
+            <?php endif; ?>     
+        <?php endif; ?>
+        
             <?php comment_id_fields(); ?>
 
-            <?php if($user_ID) : ?>
-				<p>Conectado como <a href="<?php print get_option('siteurl'); ?>/wp-admin/profile.php"><?php print $user_identity; ?></a>. <a href="<?php print get_option('siteurl'); ?>/wp-login.php?action=logout" title="Logout">Logout &raquo;</a></p>
-			
-			<?php endif; ?>                     
+                           
 			<textarea name="comment" id="comment" tabindex="1" onfocus="if (this.value == 'Insira seu comentário aqui.') this.value = '';" onblur="if (this.value == '') {this.value = 'Insira seu comentário aqui.';}">Insira seu comentário aqui.</textarea>			           
 
             <?php cancel_comment_reply_link('cancelar'); ?><input type="submit" name="comentar" id="comentar" class="submit-button" value="<?php _e('Comentar', 'consulta'); ?>" />
@@ -69,7 +80,7 @@ add_filter('comment_class','comment_add_microid');
             <?php endif; ?>
             <?php do_action('comment_form', $post->ID); ?>
         </form>
-        <?php endif; ?>
+        
     </div>
     <?php endif; ?>
 </div>
