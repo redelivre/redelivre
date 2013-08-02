0674c374eafc35bf6034b02a2b013c13c8ece996
Juliano Amaral <juliano@ethymos.com.br>
2013-08-02 10:41:25 -0300

    substituição por placeholder no searchform.php

--- src/wp-content/themes/mobilize/searchform.php
+++ src/wp-content/themes/mobilize/searchform.php
@@ -1,3 +1,3 @@
 <form action="<?php echo home_url() ?>" method="get" id="searchform">
-	<input type="text" name="s" value="Digite sua busca e aperte enter" onFocus="this.value=''" />
+	<input type="text" name="s" placeholder="Digite sua busca e aperte enter" />
 </form>
