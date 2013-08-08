db530096a4d1662d424d1381a777caacbe181a1a
Juliano Amaral <juliano@ethymos.com.br>
2013-08-08 18:17:31 -0300

    removida dependências do tema campanha

--- src/wp-content/themes/campanha/includes/campaigns.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_GET['action']) && $_GET['action'] == 'edit') {
-    // edit campaign
-    require_once('campaigns_edit.php');
-} else {
-    // list or delete campaigns
-    require_once('campaigns_list.php');
-} 
