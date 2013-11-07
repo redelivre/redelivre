1bfedeefc1482b8b1ebece06797e1584f1d2b8c2
Jacson <jacson@ethymos.com.br>
2013-11-07 11:08:45 -0200

    teste do e-mail, remover

---
+++ mail_test.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once 'src/wp-includes/class-phpmailer.php';
+
+$phpmailer = new PHPMailer(true);
+
+$phpmailer->AddReplyTo('jacson@passold.net.br');
+
+$phpmailer->From     = "test@redelivre.org";
+$phpmailer->FromName = "Test";
+
+$phpmailer->AddAddress('jacson@ethymos.com.br', "Jacson");
+
+$phpmailer->Subject = 'Test 123';
+$phpmailer->Body    = 'Testando 123';
+
+$phpmailer->IsMail();
+
+$phpmailer->ContentType = 'text/plain';
+
+$phpmailer->CharSet = 'UTF8';
+
+$phpmailer->
+
+try {
+	$phpmailer->Send();
+} catch ( phpmailerException $e ) {
+	echo '<pre>Error\n';
+	var_dump($e);
+	echo '</pre>';
+}
+
+echo 'OK';
+
+?>
