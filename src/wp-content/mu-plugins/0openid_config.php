e06f7f20572836a8cf86924c68b8e007e6e80baa
Flávio Zavan <flaviozavan@gmail.com>
2014-01-16 13:37:32 -0200

    Coloca as configuração do openid no mu-plugins

---
+++ src/wp-content/mu-plugins/0openid_config.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Plugins are loaded in alphabetical order. We want this to the be loaded
+ * early. */
+
+define('OPENID_ENABLE_CONSUMER', false);
+define('OPENID_ENABLE_COMMENTS', false);
+
+?>
