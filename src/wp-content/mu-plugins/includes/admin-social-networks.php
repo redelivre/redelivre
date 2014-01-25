41c4844c8adfd9020be429d34dab82c0c6e306fb
Eduardo Zulian <eduardozulian@gmail.com>
2014-01-25 17:02:08 -0200

    Adiciona campo para o Flickr
    Aproveita e atualiza o código para que o layout fique compatível com o
    backend do WP

--- src/wp-content/mu-plugins/includes/admin-social-networks.php
+++ src/wp-content/mu-plugins/includes/admin-social-networks.php
@@ -1,35 +1,54 @@
 <?php
 $msg = "";
-if (isset($_POST['admin_social_networks']) && wp_verify_nonce($_POST['admin_social_networks'], 'admin_social_networks')) {
-    $msg = "<div class='updated settings-error'>As configurações foram salvas</div>";
-    update_option('campanha_social_networks', $_POST['social_networks']);
+if ( isset( $_POST['admin_social_networks'] ) && wp_verify_nonce( $_POST['admin_social_networks'], 'admin_social_networks' ) ) {
+    $msg = '<div class="updated settings-error"><p>';
+    $msg .= __( 'Settings saved.' );
+    $msg .= '</p></div>';
+    update_option( 'campanha_social_networks', $_POST['social_networks'] );
 }
 
-$option = get_option('campanha_social_networks');
+$option = get_option( 'campanha_social_networks' );
 ?>
-<style>
-    label { display:inline-block; width:150px; }
-    input[type="text"] { width: 350px; }
-    
-</style>
+
 <div class="wrap">
     <div id="icon-options-general" class="icon32"><br/></div>
     <h2>Redes Sociais</h2>
     <form method="post">
+        <?php settings_fields( 'campaign_social_networks' ); ?>
         <?php echo $msg; ?>
         <?php wp_nonce_field('admin_social_networks', 'admin_social_networks'); ?>
-        <p>
-            <label for="rede-0">Página do Facebook:</label> <input id="rede-0" type="text" name="social_networks[facebook-page]" value="<?php echo @htmlentities($option['facebook-page']) ?>"/><br/>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row"><label for="rede-0">Página do Facebook</label></th>
+                <td><input id="rede-0" type="text" name="social_networks[facebook-page]" value="<?php echo @htmlentities($option['facebook-page']) ?>" class="regular-text" /></td>
+            </tr>
 
-            <label for="rede-1">Perfil do Facebook:</label> <input id="rede-1" type="text" name="social_networks[facebook]" value="<?php echo @htmlentities($option['facebook']) ?>"/><br/>
+            <tr valign="top">
+                <th scope="row"><label for="rede-1">Perfil do Facebook</label></th>
+                <td><input id="rede-1" type="text" name="social_networks[facebook]" value="<?php echo @htmlentities($option['facebook']) ?>" class="regular-text" /></td>
+            </tr>
 
-            <label for="rede-2">Twitter:</label> <input id="rede-2" type="text" name="social_networks[twitter]" value="<?php echo @htmlentities($option['twitter']) ?>"/><br/>
+            <tr valign="top">
+                <th scope="row"><label for="rede-2">Twitter</label></th>
+                <td><input id="rede-2" type="text" name="social_networks[twitter]" value="<?php echo @htmlentities($option['twitter']) ?>" class="regular-text" /></td>
+            </tr>
 
-            <label for="rede-3">Google+:</label> <input id="rede-3" type="text" name="social_networks[google]" value="<?php echo @htmlentities($option['google']) ?>"/><br/>
-            
-            <label for="rede-4">Youtube:</label> <input id="rede-4" type="text" name="social_networks[youtube]" value="<?php echo @htmlentities($option['youtube']) ?>"/><br/>
-        </p>
-        <input type="submit" name="submit" class="button-primary" value="Salvar configurações" />
+            <tr valign="top">
+                <th scope="row"><label for="rede-3">Google+</label></th>
+                <td><input id="rede-3" type="text" name="social_networks[google]" value="<?php echo @htmlentities($option['google']) ?>" class="regular-text" /></td>
+            </tr>
 
+            <tr valign="top">
+                <th scope="row"><label for="rede-4">YouTube</label></th>
+                <td><input id="rede-4" type="text" name="social_networks[youtube]" value="<?php echo @htmlentities($option['youtube']) ?>" class="regular-text" /></td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row"><label for="rede-5">Flickr</label></th>
+                <td><input id="rede-5" type="text" name="social_networks[flickr]" value="<?php echo @htmlentities($option['flickr']) ?>" class="regular-text" /></td>
+            </tr>
+        </table>
+        <?php do_settings_sections( 'campaign_social_networks' ); ?>
+        <?php submit_button(); ?>
     </form>
 </div>
