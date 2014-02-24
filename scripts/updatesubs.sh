f8cceb0c8fce112aadc719f6870d8759f37acc92
Jacson Passold <jacson@ethymos.com.br>
2014-02-24 11:36:08 -0300

    initial script for correct update submodules

---
+++ scripts/updatesubs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PWDAtual=`pwd`
+
+git pull;
+
+for l in $(git submodule|grep -v wordpress-xrds-simple|grep -v wordpress-openid|grep -v mapasdevista |awk {'print $2;'}); do
+        cd $PWDAtual/$l;
+        git checkout master;
+        git pull;
+done
+cd $PWDAtual/src/wp-content/plugins/mapasdevista;
+git checkout redelivre;
+git pull;
