79dfc0e63caca27f7071b979c3c5b09a09d4a2de
Jacson Passold <jacson@ethymos.com.br>
2014-04-15 12:05:30 -0300

    wp 3.8.3

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8.2';
+$wp_version = '3.8.3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 26691;
+$wp_db_version = 26692;
 
 /**
  * Holds the TinyMCE version
@@ -33,3 +33,5 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
+
+$wp_local_package = 'pt_BR';
