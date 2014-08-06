c6674992fb91c2f46d41a9046214511d6cfe376e
Jacson Passold <jacson@ethymos.com.br>
2014-08-06 17:15:50 -0300

    try to fix android download error on mp3

--- src/wp-content/mu-plugins/includes/audio-player/download_audio.php
+++ src/wp-content/mu-plugins/includes/audio-player/download_audio.php
@@ -3,14 +3,14 @@
 $file_url = $_GET['file_url'];
     
 $clean_name = str_replace(dirname($file_url) . '/', '', $file_url);
-$clean_name = preg_replace('/(-[0-9a-f]{13}.mp3)$/', '.mp3', $clean_name);
+$clean_name = preg_replace('/(-[0-9a-f]{13}.mp3)$/', '.MP3', $clean_name);
 
 if ($file_url) {
     
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
     header("Content-Type: 'audio/mpeg3'");
-    header("Content-Disposition: attachment; filename=$clean_name");
+    header("Content-Disposition: attachment; \"filename=$clean_name\"");
     header("Content-Transfer-Encoding: binary");
 
     readfile($file_url);
