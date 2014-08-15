b8afc60f8db1b33b8893e4525670900d93d46a33
Jacson Passold <jacson@ethymos.com.br>
2014-08-15 17:11:54 -0300

    fix recid branch

--- scripts/updatesubs.sh
+++ scripts/updatesubs.sh
@@ -6,7 +6,7 @@ git pull;
 
 for l in $(git submodule |grep ^-|awk '{print $2}'); do git submodule update --init $l; done
 
-for l in $(git submodule|grep -v wordpress-xrds-simple|grep -v wordpress-openid|grep -v mapasdevista |awk {'print $2;'}); do
+for l in $(git submodule|grep -v wordpress-xrds-simple|grep -v wordpress-openid|grep -v mapasdevista| grep -v recid |awk {'print $2;'}); do
 	echo "Updating $l"
         cd $PWDAtual/$l;
         git checkout master;
@@ -24,6 +24,10 @@ fi
 
 cd $PWDAtual/src/wp-content/plugins/wordpress-openid
 git checkout anyone
+git pull
+
+cd $PWDAtual/src/wp-content/themes/recid
+git checkout recid
 git pull
 
 cd $PWDAtual
