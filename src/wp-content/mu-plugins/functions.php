849a96a7a3ce8a24d267d3be289b4eed57e1df69
Jacson Passold <jacson@ethymos.com.br>
2016-06-08 15:07:46 -0300

    fix lost password link

--- src/wp-content/mu-plugins/functions.php
+++ src/wp-content/mu-plugins/functions.php
@@ -688,72 +688,65 @@ function lost_password_reset(){
 	//url para onde ele encaminha a validação do link por email, é preciso alterar.
 	$validate_url = get_bloginfo('url').'/'.lost_password_page;
 	
-	if($user_info){
-				
+	if($user_info)
+	{
+		add_filter( 'wp_mail_content_type', function( $content_type ) {
+			return 'text/html';
+		});
 		/*
 		* Daqui para a frente eu fiz com base na função original do wordpress.
 		*/
-		// Generate something random for a password reset key.
-		$key = wp_generate_password( 20, false );
-		
-		/**
-		 * Fires when a password reset key is generated.
-		 *
-		 * @since 2.5.0
-		 *
-		 * @param string $user_login The username for the user.
-		 * @param string $key        The generated password reset key.
-		*/
-		do_action( 'retrieve_password_key', $user_login, $key );
-		
-		// Now insert the key, hashed, into the DB.
-		if ( empty( $wp_hasher ) ) {
-			require_once ABSPATH . 'wp-includes/class-phpass.php';
-			$wp_hasher = new PasswordHash( 8, true );
+		$key = get_password_reset_key( $user_data );
+
+		if ( is_wp_error( $key ) ) {
+			return $key;
 		}
-		$hashed = $wp_hasher->HashPassword( $key );
-		$wpdb->update( $wpdb->users, array( 'user_activation_key' => $hashed ), array( 'user_login' => $user_login ) );
-		
-		$message = __('Someone requested that the password be reset for the following account:') . "\r\n\r\n";
+	
+		$message = __('Someone has requested a password reset for the following account:') . "\r\n\r\n";
 		$message .= network_home_url( '/' ) . "\r\n\r\n";
 		$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
 		$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
 		$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
-		$url = network_site_url("wp-login.php?action=rp&key=$key&login="
-				. rawurlencode($user_login), 'login');
-		$message .= "<a href=\"$url\" target=\"_blank\">$url</a>\r\n";
-		
+		$message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\n";
+	
 		if ( is_multisite() )
 			$blogname = $GLOBALS['current_site']->site_name;
 		else
-			// The blogname option is escaped with esc_html on the way into the database in sanitize_option
-			// we want to reverse this for the plain text arena of emails.
+			/*
+			 * The blogname option is escaped with esc_html on the way into the database
+			 * in sanitize_option we want to reverse this for the plain text arena of emails.
+			 */
 			$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
-		
+	
 		$title = sprintf( __('[%s] Password Reset'), $blogname );
-		
+	
 		/**
 		 * Filter the subject of the password reset email.
 		 *
 		 * @since 2.8.0
+		 * @since 4.4.0 Added the `$user_login` and `$user_data` parameters.
 		 *
-		 * @param string $title Default email title.
-		*/
-		$title = apply_filters( 'retrieve_password_title', $title );
+		 * @param string  $title      Default email title.
+		 * @param string  $user_login The username for the user.
+		 * @param WP_User $user_data  WP_User object.
+		 */
+		$title = apply_filters( 'retrieve_password_title', $title, $user_login, $user_data );
+	
 		/**
 		 * Filter the message body of the password reset mail.
 		 *
 		 * @since 2.8.0
+		 * @since 4.1.0 Added `$user_login` and `$user_data` parameters.
 		 *
-		 * @param string $message Default mail message.
-		 * @param string $key     The activation key.
-		*/
-		$message = apply_filters( 'retrieve_password_message', $message, $key );
-		
-		add_filter('wp_mail_content_type',create_function('', 'return "text/html"; '));
-		
-		if ( $message && !wp_mail($user_email, $title, $message) )
-			wp_die( __('The e-mail could not be sent.') . "<br />\n" . __('Possible reason: your host may have disabled the mail() function.') );
+		 * @param string  $message    Default mail message.
+		 * @param string  $key        The activation key.
+		 * @param string  $user_login The username for the user.
+		 * @param WP_User $user_data  WP_User object.
+		 */
+		$message = apply_filters( 'retrieve_password_message', $message, $key, $user_login, $user_data );
+	
+		if ( $message && !wp_mail( $user_email, wp_specialchars_decode( $title ), $message ) )
+			wp_die( __('The email could not be sent.') . "<br />\n" . __('Possible reason: your host may have disabled the mail() function.') );
 		/*
 		* Fim do ctrl+c do wordpress
 		*/
