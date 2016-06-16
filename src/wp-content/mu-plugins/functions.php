40b463aa683175a81e91186129dbb7b4de2ad736
Jacson Passold <jacson@ethymos.com.br>
2016-06-16 14:04:31 -0300

    fix url from lost password blog where it come from

--- src/wp-content/mu-plugins/functions.php
+++ src/wp-content/mu-plugins/functions.php
@@ -703,11 +703,11 @@ function lost_password_reset(){
 		}
 	
 		$message = __('Someone has requested a password reset for the following account:') . "\r\n\r\n";
-		$message .= network_home_url( '/' ) . "\r\n\r\n";
+		$message .= home_url( '/' ) . "\r\n\r\n";
 		$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
 		$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
 		$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
-		$message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\n";
+		$message .= site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\n";
 	
 		if ( is_multisite() )
 			$blogname = $GLOBALS['current_site']->site_name;
