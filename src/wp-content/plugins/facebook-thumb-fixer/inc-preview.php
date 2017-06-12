d570c13cdf81d1bf7d4d9970914b7649c182b91e
Jacson Passold <jacson@ethymos.com.br>
2017-06-12 17:33:34 -0300

    fix some errors

--- src/wp-content/plugins/facebook-thumb-fixer/inc-preview.php
+++ src/wp-content/plugins/facebook-thumb-fixer/inc-preview.php
@@ -18,49 +18,54 @@ $image_height = $image_data[2];
 <?php } else { ?>
 	<a class="no-thumb-set">Preview Not Available</a>
 	<p>You can't preview until you set a featured image and update this post/page.</p>
-<?php } ?>
-
-<div class="ftf-live-preview">
-	<img src="<?php echo get_option('siteurl').'/wp-content/plugins/'.basename(dirname(__FILE__)).'/images/preview-top.png'; ?>" />
-
-	<div class="ftf-preview-details">
-
-		<div class="overflow<?php if ($image_width < 600 || $image_height < 315) { echo " too-small"; } ?>">
-			<?php echo the_post_thumbnail( $post->ID, 'full' ); ?>
+<?php }
+if ( has_post_thumbnail() ) {
+?>
+	
+	<div class="ftf-live-preview">
+		<img src="<?php echo get_option('siteurl').'/wp-content/plugins/'.basename(dirname(__FILE__)).'/images/preview-top.png'; ?>" />
+	
+		<div class="ftf-preview-details">
+	
+			<div class="overflow<?php if ($image_width < 600 || $image_height < 315) { echo " too-small"; } ?>">
+				<?php echo the_post_thumbnail( $post->ID, 'full' ); ?>
+			</div>
+	
+			<h1><?php echo the_title(); ?></h1>
+	
+			<p>
+				<?php
+					if ( has_excerpt( $post->ID ) ) {
+						$excerpt = get_the_excerpt();
+						$excerpt_chars = substr($excerpt, 0, 150);
+						echo strip_tags($excerpt_chars);
+					} else {
+						$post = get_post($post->ID);
+						$content = apply_filters('the_content', $post->post_content);
+						$content_chars = substr($content, 0, 150);
+						echo strip_tags($content_chars);
+					}
+				?>
+			</p>
+			<span class="ftf-domain"><?php echo $_SERVER['SERVER_NAME']; ?></span>
 		</div>
-
-		<h1><?php echo the_title(); ?></h1>
-
-		<p>
-			<?php
-				if ( has_excerpt( $post->ID ) ) {
-					$excerpt = get_the_excerpt();
-					$excerpt_chars = substr($excerpt, 0, 150);
-					echo strip_tags($excerpt_chars);
-				} else {
-					$post = get_post($post->ID);
-					$content = apply_filters('the_content', $post->post_content);
-					$content_chars = substr($content, 0, 150);
-					echo strip_tags($content_chars);
-				}
-			?>
-		</p>
-		<span class="ftf-domain"><?php echo $_SERVER['SERVER_NAME']; ?></span>
 	</div>
-</div>
-
-<div class="ftf-mask"></div>
-
-<script>
-	// Popup
-	jQuery(function($) {
-		$('.open-ftf-preview').click(function(){
-			$(".ftf-live-preview").toggleClass("show-ftf-live-preview");
-			$(".ftf-mask").toggleClass("show-ftf-mask");
+	
+	<div class="ftf-mask"></div>
+	
+	<script>
+		// Popup
+		jQuery(function($) {
+			$('.open-ftf-preview').click(function(){
+				$(".ftf-live-preview").toggleClass("show-ftf-live-preview");
+				$(".ftf-mask").toggleClass("show-ftf-mask");
+			});
+			$('.ftf-mask').click(function(){
+				$(this).toggleClass("show-ftf-mask");
+				$(".ftf-live-preview").toggleClass("show-ftf-live-preview");
+			});
 		});
-		$('.ftf-mask').click(function(){
-			$(this).toggleClass("show-ftf-mask");
-			$(".ftf-live-preview").toggleClass("show-ftf-live-preview");
-		});
-	});
-</script>
+	</script>
+<?php
+}
+?>
