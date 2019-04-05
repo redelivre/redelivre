949060e1603b52023531abd28de61ce976f4ab4a
Jacson Passold <jacson@passold.com.br>
2019-04-05 15:26:36 -0300

    try to send wp captcha code too and check if elements exists

--- src/wp-content/mu-plugins/includes/custom-lost-password.php
+++ src/wp-content/mu-plugins/includes/custom-lost-password.php
@@ -1,55 +1,56 @@
-
-<h1>
-	<?php echo _x('Recuperar minha senha', 'custom-lost-password', 'redelivre'); ?>
-</h1>
-
-<?php if(!isset($_GET['action']) || $_GET['action'] == 'lostpassword') : ?>
-
-<form>
-
-	<p>
-		<?php echo _x('Digite seu nome de usuário ou endereço de email. Você receberá um link para criar uma nova senha via email.', 'custom-lost-password', 'redelivre'); ?>
-	</p>
+<div class="custom-lost-password-content">
+	<h1>
+		<?php echo _x('Recuperar minha senha', 'custom-lost-password', 'redelivre'); ?>
+	</h1>
 	
-	<p>
-		<label for="user-email"><?php _e('Digite seu email utilizado no cadastro', 'custom-lost-password', 'redelivre'); ?></label><br />
-		<input type="text" name="user-email" id="user-email">
-	</p>
-	<div class="lostpassword_form_action">
-		<?php do_action('lostpassword_form'); ?>
-	</div>
-	<p>
-		<input type="button" value="enviar" id="lost-password-send"/>
-	</p>
+	<?php if(!isset($_GET['action']) || $_GET['action'] == 'lostpassword') : ?>
 	
-	<div class="resposta-ajax">
+	<form>
+	
+		<p>
+			<?php echo _x('Digite seu nome de usuário ou endereço de email. Você receberá um link para criar uma nova senha via email.', 'custom-lost-password', 'redelivre'); ?>
+		</p>
 		
-	</div>
+		<p>
+			<label for="user-email"><?php _e('Digite seu email utilizado no cadastro', 'custom-lost-password', 'redelivre'); ?></label><br />
+			<input type="text" name="user-email" id="user-email">
+		</p>
+		<div class="lostpassword_form_action">
+			<?php do_action('lostpassword_form'); ?>
+		</div>
+		<p>
+			<input type="button" value="enviar" id="lost-password-send"/>
+		</p>
 		
-</form>
-<?php elseif($_GET['action'] == 'rp' || $_GET['action'] == 'resetpass'): ?>
-<form class="custom-lost-password">
-	<input type="hidden" id="user_login" value="<?php echo esc_attr( $_GET['login'] ); ?>" autocomplete="off" />
-	<input type="hidden" id="key" value="<?php echo esc_attr( $_GET['key'] ); ?>" autocomplete="off" />
-
-	<p>
-		<label for="pass1"><?php _e('New password') ?><br />
-		<input type="password" name="pass1" id="pass1" size="20" value="" autocomplete="off" /></label>
-	</p>
-	<p>
-		<label for="pass2"><?php _e('Confirm new password') ?><br />
-		<input type="password" name="pass2" id="pass2" size="20" value="" autocomplete="off" /></label>
-	</p>
-
-	<div id="pass-strength-result" class="hide-if-no-js"><?php _e('Strength indicator'); ?></div>
-	<p class="description indicator-hint"><?php _e('Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).'); ?></p>
-
-	<br class="clear" />
-	<input type="button" value="<?php esc_attr_e('Reset Password'); ?>" id="lost-password-send-reset"/>
+		<div class="resposta-ajax">
+			
+		</div>
+			
+	</form>
+	<?php elseif($_GET['action'] == 'rp' || $_GET['action'] == 'resetpass'): ?>
+	<form class="custom-lost-password">
+		<input type="hidden" id="user_login" value="<?php echo esc_attr( $_GET['login'] ); ?>" autocomplete="off" />
+		<input type="hidden" id="key" value="<?php echo esc_attr( $_GET['key'] ); ?>" autocomplete="off" />
 	
-	<div class="resposta-ajax">
+		<p>
+			<label for="pass1"><?php _e('New password') ?><br />
+			<input type="password" name="pass1" id="pass1" size="20" value="" autocomplete="off" /></label>
+		</p>
+		<p>
+			<label for="pass2"><?php _e('Confirm new password') ?><br />
+			<input type="password" name="pass2" id="pass2" size="20" value="" autocomplete="off" /></label>
+		</p>
 	
-	</div>
-</form>
-<br/>
-<?php endif; ?>
+		<div id="pass-strength-result" class="hide-if-no-js"><?php _e('Strength indicator'); ?></div>
+		<p class="description indicator-hint"><?php _e('Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).'); ?></p>
+	
+		<br class="clear" />
+		<input type="button" value="<?php esc_attr_e('Reset Password'); ?>" id="lost-password-send-reset"/>
+		
+		<div class="resposta-ajax">
+		
+		</div>
+	</form>
+	<br/>
+	<?php endif; ?>
+</div>
