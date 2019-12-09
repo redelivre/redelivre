4fd81fd1ff4b7c7f9a7936126391f40a217c2873
Jacson Passold <jacson@passold.com.br>
2019-12-09 15:17:16 -0300

    using browser base url for ga js include

--- src/wp-content/plugins/google-analytics-multisite-async/ga-mu-async.php
+++ src/wp-content/plugins/google-analytics-multisite-async/ga-mu-async.php
@@ -246,7 +246,7 @@ if ( !function_exists('ga_mu_plugin_add_script_to_head') ) :
 				?>
 				(function() {
 					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					ga.src = '//www.google-analytics.com/ga.js';
 					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 				  })();
 				</script>			
